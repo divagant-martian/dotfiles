fcd023ed079fe51847b2d2f623db53b961730ea6
Diva Martínez <git@divma.net>
2026-06-08 21:10:06 -0500

    claude has OS theme detection

--- fish/functions/claude.fish
+++
@@ -1,9 +0,0 @@
-function claude
-    set gnome_scheme (gsettings get org.gnome.desktop.interface color-scheme 2>/dev/null)
-    if string match -q '*dark*' $gnome_scheme
-        set theme dark-daltonism
-    else
-        set theme light-daltonism
-    end
-    command claude --settings "{\"theme\": \"$theme\"}" $argv
-end
